b89ee8cb6242520fcb9de5ddb4c8c043202050ab
Stanislav Sheludyakov <serophiom@gmail.com>
2021-04-10 00:52:47 +0300

    добавляет выполнение второго ДЗ

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+  
+const potatoEl = document.createElement('li');
+potatoEl.textContent = ingredients[0];
+const mushroomEl = document.createElement('li');
+mushroomEl.textContent = ingredients[1];
+const garlicEl = document.createElement('li');
+garlicEl.textContent = ingredients[2];
+const tomatoEl = document.createElement('li');
+tomatoEl.textContent = ingredients[3];
+const greensEl = document.createElement('li');
+greensEl.textContent = ingredients[4];
+const condimentsEl = document.createElement('li');
+condimentsEl.textContent = ingredients[5];
+
+const ulRef = document.querySelector('ul');
+ulRef.append(potatoEl, mushroomEl, garlicEl, tomatoEl, greensEl, condimentsEl);
