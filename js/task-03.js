977dc172e2f249173a5d5b4cd8de769a750ece45
Stanislav Sheludyakov <serophiom@gmail.com>
2021-04-21 06:42:01 +0300

    8 ДЗ

--- js/task-03.js
+++ js/task-03.js
@@ -18,87 +18,10 @@ const images = [
 
 const imageListRef = document.querySelector('#gallery');
 
+const imageRefs = images.map(image => {
+  const imageLiRef = document.createElement('li');
+  imageLiRef.insertAdjacentHTML('beforeend', `<img src="${image.url}" alt="${image.alt}">`);
+  return imageLiRef;
+});
 
-// const imageRefs = images.map(image => {
-//   const imgRef = document.createElement('img');
-//   console.log(imgRef);
-//   imgRef.src = image.url;
-//   imgRef.alt = image.alt;
-//   return imgRef;
-// });
-
-
-
-  const imageRefs = images.map(image => {
-    const imgRef = imageListRef.insertAdjacentHTML('beforeend', '<li><img></li>');
-    const imgRefs = document.querySelector('img');
-    imgRefs.src = image.url;
-    imgRefs.alt = image.alt;
-    console.log(imgRef);
-    
-    return imgRef;
-  });
-
-
-console.log(imageRefs);
-console.log(imageListRef);
-
-// console.log(imageListRef);
-
-// const vagetablesRef = ingredients.map(item => {
-//     const vagetabelsItem = document.createElement('li');
-//     vagetabelsItem.textContent = item;
-//     return vagetabelsItem;
-//   });
-
-
-// imageListRef.insertAdjacentHTML('beforeend', '<li><img></li><li><img></li><li><img></li>');
-
-// const imgRefs = imageListRef.querySelectorAll('img');
-
-// console.log(imgRefs);
-// images.forEach ( image => {
-//   imgRefs.src = images.url;
-// imgRefs[0].alt = images[0].alt;
-// imgRefs[1].src = images[1].url;
-// imgRefs[1].alt = images[1].alt;
-// imgRefs[2].src = images[2].url;
-// imgRefs[2].alt = images[2].alt;
-// return 
-// })
-
-
-// const imageListRef = document.querySelector('#gallery');
-// const liList = document.insertAdjacentHTML
-// images.forEach(image => {
-
-//   imageListRef.insertAdjacentHTML('beforeend', '<li><img src=""></li>');
-//   const imgEl = imageListRef.querySelector('img');
-//   imgEl.src = image.url;
-//   // imageListRef.img = image.url;
-//   // imageListRef.src = "image.url";
-
-//   return imageListRef;
-// });
-
-// // imageListRef.insertAdjacentHTML('afterbegin', '<li><img></li>');
-// // imageListRef.insertAdjacentHTML('beforeend', '<li><img></li>');
-// // imageListRef.insertAdjacentHTML('beforeend', '<li><img></li>');
-
-// const url1 = images[0].url;
-// const liEl = document.createElement('li');
-// const imgEl = document.createElement('img');
-
-// imgEl.src = url1;
-// // imageListRef.append(liEl, imgEl);
-
-// liEl.appendChild(imgEl);
-// console.log(url1);
-
-// // imageListRef.appendChild(liEl);
-
-// imageListRef.insertAdjacentHTML('beforeend', '<li><img class="image1"></li>');
-
-// console.log(imageListRef);
-// console.log(images[0].url);
-// console.log(images[0].alt);
+imageListRef.append(...imageRefs);
--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,5 @@
 const inputRef = document.querySelector('input');
+const boxesRef = document.querySelector('#boxes')
 const btnRefs = document.querySelectorAll('button');
 const btnCreateRef = btnRefs[0];
 const btnDeleteRef = btnRefs[1];
@@ -8,9 +9,20 @@ console.log(divCol);
 console.log(btnRefs);
 console.log(btnCreateRef);
 console.log(btnDeleteRef);
+console.log(boxesRef);
 
-function createBoxes(amount) {
+const divRef = createBoxes();
 
+boxesRef.insertAdjacentHTML('beforeend', divRef);
+
+document.createElement('div');
+
+
+function createBoxes() {
+    return `
+    <div style="background-color: #f44336">
+    </div>
+    `;
 }
 
 function destroyBoxes() {
