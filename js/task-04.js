769a4db84bac51a436a32e12bbbc8ab45b66fc85
Stanislav Sheludyakov <serophiom@gmail.com>
2021-04-23 23:12:07 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -2,17 +2,13 @@ const counterValueEl = document.querySelector('#value');
 let counterValue = Number(counterValueEl.textContent);
 
 const counterBtnAll = document.querySelectorAll('button');
-console.log(counterBtnAll[0]);
-console.log(counterBtnAll[1]);
 
 function increment () {
-    console.log('Это increment');
     counterValue += 1;
     counterValueEl.textContent = counterValue;
 };
 
 function decrement () {
-    console.log('Это decrement');
     counterValue -= 1;
     counterValueEl.textContent = counterValue;   
 };
